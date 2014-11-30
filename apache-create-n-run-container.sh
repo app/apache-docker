faa6ee356f07e7a13b66b008fb532257ed15eabd
Andrey Paskal <apaskal@gmail.com>
2014-11-30 18:15:08 +0300

    Download image from docker hub if not exists

--- apache-create-n-run-container.sh
+++ apache-create-n-run-container.sh
@@ -1,1 +1,1 @@
-sudo docker run -t -d -p 127.0.0.1:80:80 -v `pwd`/http_root:/var/www:ro ubuntu/apache2
+sudo docker run -t -d -p 127.0.0.1:80:80 -v `pwd`/http_root:/var/www:ro apaskal/ubuntu_apache2
